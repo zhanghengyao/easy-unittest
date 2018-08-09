2f252493f677dedff66a9b221adc365892594c8e
恒遥 <hengyao.zhy@alibaba-inc.com>
2018-08-09 11:47:24 +0800

    feat: config

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
 const start = require('./lib');
 start({
-  entry:'../../lib/**/**/*.js',
+  entry:'../../demo/**/**/*.js',
   output: '../../test/',
   injections: {
-    headerBlocks:[
-      `const assert = require('assert')`,
-      'const mockResult = { data: [123] }'
-    ],
-    specBlocks: [
-      `console.log('fuck')`,
-      `assert(ret === mockResult);`
+    headerBlocks:[{
+      path: '',
+      blocks: [
+        `const assert = require('assert')`,
+      ]
+    },{
+      path: /controller/,
+      blocks: [
+        `const mockResult = { data: [123] }`
+      ]
+    },{
+      path: /service/,
+      blocks: [
+        `const mockResult = { data: [666] }`
+      ]
+    }],
+    suiteBlocks: [{
+      path: /controller/,
+      blocks: [
+        ` beforeEach(async function() {
+          this.testHelper.middlewarePrepare.loginPrepare();
+        });`,
+        `const apiUrl = '/dashboard/rank_special';`
+      ]
+    }],
+    specBlocks: [{
+        path: /controller|service/,
+        method: /^(?!pageEntry)/,
+        blocks: [
+          `assert(ret === mockResult);`
+        ]
+      },{
+        path: /controller/,
+        method: /pageEntry/,
+        blocks: [
+          `assert(ret.status === 200);`
+        ]
+      }
     ]
-  }
+  },
+  dependencies: [{
+    rule: /service/g,
+    mock: `this.app.mockServiceArgs(CALLEE, METHOD, (...params) => {
+      
+    })`
+  }]
 });
