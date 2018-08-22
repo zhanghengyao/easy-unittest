7c42e46b40764e2935424d298df2805a0e8e1b13
恒遥 <hengyao.zhy@alibaba-inc.com>
2018-08-22 17:14:38 +0800

    feat: change quotes double to single

--- demo/controller/home.test.js
+++ demo/controller/home.test.js
@@ -1,3 +1,3 @@
-describe("/Users/zhangyao/Documents/work/easy-unittest/fixtures/controller/home.js", () => {
-  describe("pageEntry", () => {});
+describe('/Users/zhangyao/Documents/work/easy-unittest/fixtures/controller/home.js', () => {
+  describe('pageEntry', () => {});
 });
